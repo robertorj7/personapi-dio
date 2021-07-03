b565b8f2f4912aefae09c057df75ea92f3e832af
Roberto R Jr <rrjr87@outlook.com>
2021-07-03 09:33:51 -0300

    reset struture from services

--- src/main/java/one/digitalinnovation/personapi/controllers/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controllers/PersonController.java
@@ -1,31 +1,31 @@
 package one.digitalinnovation.personapi.controllers;
 
-import lombok.AllArgsConstructor;
-import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
-import one.digitalinnovation.personapi.exception.PersonNotFoundException;
-import one.digitalinnovation.personapi.services.PersonService;
+import one.digitalinnovation.personapi.entities.Person;
+import one.digitalinnovation.personapi.repositories.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
-    @GetMapping
-    public String getPerson() {
-     return "API Test!";
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    @PostMapping
+    public MessageResponseDTO createPerson(@RequestBody Person person) {
+     Person savedPerson = personRepository.save(person);
+     return MessageResponseDTO
+             .builder()
+             .message("Created person with Id " + savedPerson.getId())
+             .build();
     }
 }
