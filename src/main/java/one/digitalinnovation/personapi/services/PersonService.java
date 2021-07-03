071cb9b6cc5a4cc9940f8a6b0f2c48c2cd80cb0b
Roberto R Jr <rrjr87@outlook.com>
2021-07-03 18:02:38 -0300

    Developed delete by id operation

--- src/main/java/one/digitalinnovation/personapi/services/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/services/PersonService.java
@@ -42,8 +42,19 @@ public class PersonService {
     }
 
     public PersonDTO findById(Long id) throws PersonNotFoundException {
-        Person person = personRepository.findById(id)
-                .orElseThrow(() -> new PersonNotFoundException(id));
+        Person person = verifyIfExists(id);
         return personMapper.toDTO(person);
+    }
+
+    public void delete(Long id) throws PersonNotFoundException {
+        personRepository.findById(id)
+                .orElseThrow(() -> new PersonNotFoundException((id)));
+
+        personRepository.deleteById(id);
+    }
+
+    private Person verifyIfExists(Long id) throws PersonNotFoundException {
+        return personRepository.findById(id)
+                .orElseThrow(() -> new PersonNotFoundException(id));
     }
 }
