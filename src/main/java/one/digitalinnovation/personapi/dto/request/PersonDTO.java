6c0f0c7a7a201424ffe3b6d3884bd3d93491c74d
Roberto R Jr <rrjr87@outlook.com>
2021-07-03 12:36:06 -0300

    reset dto structure

--- src/main/java/one/digitalinnovation/personapi/dto/request/PersonDTO.java
+++ src/main/java/one/digitalinnovation/personapi/dto/request/PersonDTO.java
@@ -4,12 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.br.CPF;
 
-import javax.validation.Valid;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
@@ -20,22 +15,13 @@ public class PersonDTO {
 
     private Long id;
 
-    @NotEmpty
-    @Size(min = 2, max = 100)
     private String firstName;
 
-    @NotEmpty
-    @Size(min = 2, max = 100)
     private String lastName;
 
-    @NotEmpty
-    @CPF
     private String cpf;
 
-    @NotNull
     private String birthDate;
 
-    @Valid
-    @NotEmpty
     private List<PhoneDTO> phones;
 }
--- src/main/java/one/digitalinnovation/personapi/dto/request/PhoneDTO.java
+++ src/main/java/one/digitalinnovation/personapi/dto/request/PhoneDTO.java
@@ -3,14 +3,9 @@ package one.digitalinnovation.personapi.dto.request;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 import one.digitalinnovation.personapi.enums.PhoneType;
 
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Size;
 
 @Data
 @Builder
@@ -20,10 +15,7 @@ public class PhoneDTO {
 
     private Long id;
 
-    @Enumerated(EnumType.STRING)
     private PhoneType type;
 
-    @NotEmpty
-    @Size(min = 13, max = 14)
     private String number;
 }
