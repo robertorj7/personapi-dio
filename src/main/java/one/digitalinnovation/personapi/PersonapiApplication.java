8343be65dd0e0e2567965fbdb19a17c550bcfacc
Roberto R Jr <rrjr87@outlook.com>
2021-07-05 09:28:57 -0300

    corrected initializer

--- src/main/java/one/digitalinnovation/personapi/PersonapiApplication.java
+++ src/main/java/one/digitalinnovation/personapi/PersonapiApplication.java
@@ -2,9 +2,10 @@ package one.digitalinnovation.personapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PersonapiApplication {
+public class PersonapiApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(PersonapiApplication.class, args);
